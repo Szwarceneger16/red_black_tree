6a0a096dc671e56184af61de0d0f40eb601596e3
Grzegorz Szwarc <sg44522@zut.edu.pl>
2019-10-31 11:04:29 +0100

    added graph drawing

--- node.h
+++ node.h
@@ -4,12 +4,40 @@ template<class T>
 class node
 {
 public:
+	char lp[4];
 	T value;
 	node<T>* parent;
 	node<T>* child_left;
 	node<T>* child_right;
 	bool is_red;
-	node(T val, node<T>* par) :node(val, par, true) {};
-	node(T val, node<T>* par,bool red) :value(val), parent(par), child_left(NULL), child_right(NULL),is_red(red) {};
+	node(T val, node<T>* par,char const lpp[4]) :node(val, par,lpp, true) {};
+	node(T val, node<T>* par,char const lpp[4],bool red) :value(val), parent(par), child_left(NULL), child_right(NULL),is_red(red) 
+	{
+		lp[0] = lpp[0]; lp[1] = lpp[1]; lp[2] = lpp[2]; lp[3] = lpp[3];
+	};
 };
 
+class rb_tree_exceptions
+{
+	short i;
+public:
+	explicit rb_tree_exceptions(short ii) :i(ii) {};
+	std::string get_info(void)
+	{
+		switch (this->i)
+		{
+		case 1:
+			return "Error! Finding value doesn't exist!";
+			break;
+		case 2:
+			return "Error! You fucked up! This red_black_tree is empty!!!";
+			break;
+		case 3:
+			return "Error! You pass value smaller than 0! Please Kill yoursefl";
+			break;
+		default:
+			return "I don't recognize this error";
+			break;
+		}
+	}
+};
--- rb_tree.h
+++ rb_tree.h
@@ -1,37 +1,17 @@
 #pragma once
 #include "node.h"
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
 
-class rb_tree_exceptions
-{
-	short i;
-public:
-	explicit rb_tree_exceptions(short ii):i(ii) {};
-	std::string get_info(void)
-	{
-		switch (this->i)
-		{
-		case 1:
-			return "Error! Finding value doesn't exist!";
-			break;
-		case 2:
-			return "Error! You fucked up! This red_black_tree is empty!!!";
-			break;
-		case 3:
-			return "Error! You pass value smaller than 0! Please Kill yoursefl";
-			break;
-		default:
-			return "I don't recognize this error";
-			break;
-		}
-	}
-};
 
 template<class T>
 class rb_tree
 {
 	node<T>* root;
 	long size;
+	char lp[4];
 
 	node<T>* search_position_and_add(T val, bool (*fun)(const T, const T))
 	{
@@ -42,7 +22,8 @@ class rb_tree
 			{
 				if (tmp->child_left == NULL)
 				{
-					tmp->child_left = new node<T>(val,tmp);
+					tmp->child_left = new node<T>(val,tmp,lp);
+					this->increment_lp();
 					return tmp->child_left;
 				}
 				else tmp = tmp->child_left;
@@ -51,12 +32,21 @@ class rb_tree
 			{
 				if (tmp->child_right == NULL)
 				{
-					tmp->child_right = new node<T>(val,tmp);
+					tmp->child_right = new node<T>(val,tmp,lp);
+					this->increment_lp();
 					return tmp->child_right;
 				}
 				else tmp = tmp->child_right;
 			}
 		}
+	}
+
+	void increment_lp()
+	{
+		lp[0]++;
+		if (lp[0] == 'Z') { lp[1]++; lp[0] = 'A'; }
+		if (lp[1] == 'Z') { lp[2]++; lp[1] = 'A'; }
+		if (lp[2] == 'Z') { lp[3]++; lp[2] = 'A'; }
 	}
 
 	long fall_in_tree(node<T>* tmp)
@@ -73,6 +63,33 @@ class rb_tree
 			else return(rr + 1);
 		}
 	}
+
+	void fall_in_tree_file(node<T>* tmp,std::fstream & file1,std::fstream & file2)
+	{
+			file1 << "		" << tmp->lp[3] << tmp->lp[2] << tmp->lp[1] << tmp->lp[0] << " [fillcolor=" << (tmp->is_red ? "red" : "black") << "]" << std::endl;
+			if (tmp->child_left && tmp->child_right)
+			{
+				file2 << " " << tmp->lp[3] << tmp->lp[2] << tmp->lp[1] << tmp->lp[0] << " -> {" << tmp->child_left->lp[3] << tmp->child_left->lp[2] << tmp->child_left->lp[1] << tmp->child_left->lp[0] << "," << tmp->child_right->lp[3] << tmp->child_right->lp[2] << tmp->child_right->lp[1] << tmp->child_right->lp[0] << "};" << std::endl;
+				
+				fall_in_tree_file(tmp->child_left, file1, file2);
+			
+				fall_in_tree_file(tmp->child_right, file1, file2);
+				
+			}
+			else if (tmp->child_left)
+			{
+				file2 << " " << tmp->lp[3] << tmp->lp[2] << tmp->lp[1] << tmp->lp[0] << " -> {" << tmp->child_left->lp[3] << tmp->child_left->lp[2] << tmp->child_left->lp[1] << tmp->child_left->lp[0] << "};" << std::endl;
+				fall_in_tree_file(tmp->child_left, file1, file2);
+			}
+			else if (tmp->child_right)
+			{
+				file2 << " " << tmp->lp[3] << tmp->lp[2] << tmp->lp[1] << tmp->lp[0] << " -> {" << tmp->child_right->lp[3] << tmp->child_right->lp[2] << tmp->child_right->lp[1] << tmp->child_right->lp[0] << "};" << std::endl;
+				fall_in_tree_file(tmp->child_right, file1, file2);
+			}
+			return;
+	}
+
+
 
 	void fall_in_tree_delete(node<T>* tmp)
 	{
@@ -215,9 +232,11 @@ class rb_tree
 	}
 
 public:
-	rb_tree() :root(NULL), size(0) {};
+	rb_tree() :root(NULL), size(0) { lp[0] = 'A'; lp[1] = 'A'; lp[2] = 'A'; lp[3] = 'A';
+	};
 	rb_tree(T val) :rb_tree()
 	{
+		lp[0] = 'A'; lp[1] = 'A'; lp[2] = 'A'; lp[3] = 'A';
 		add(val);
 	}
 
@@ -226,9 +245,8 @@ public:
 		this->size++;
 		if (this->root == NULL)
 		{
-			this->root = new node<T>(val, NULL,false);
-			
-			return true;
+			this->root = new node<T>(val, NULL,lp,false);
+			this->increment_lp();
 		}
 		else
 		{
@@ -262,9 +280,9 @@ public:
 				if (!tmp3->parent->parent) break;
 				tmp2 = tmp3;
 			}
-			
-			return true;
 		}
+		
+		return true;
 	}
 
 	T& find_value(T val,bool (*fun)(const T a,const T b))
@@ -323,6 +341,42 @@ public:
 		fall_in_tree_delete(this->root);
 		this->root = NULL;
 		this->size = 0;
+	}
+
+	void draw_graf()
+	{
+		std::fstream points, lines, file_out;
+		std::string line = "";
+		points.open("points.txt", std::ios::out | std::ios::in | std::ios::trunc);
+		lines.open("lines.txt", std::ios::out | std::ios::in | std::ios::trunc);
+		file_out.open("file.dot", std::ios::out | std::ios::trunc);
+		 
+		node<int>* tmp = this->root;
+
+		fall_in_tree_file(tmp,points,lines);
+
+		file_out << "digraph D {" << std::endl;
+		file_out << "	{" << std::endl;
+		file_out << "	 node [shape = circle style = filled fontcolor=yellow]" << std::endl;
+		
+		points.seekg(0, std::ios::beg);
+		while (getline(points, line)) {
+			file_out << line << std::endl;
+		}
+		file_out << "	}" << std::endl;
+		lines.seekg(0, std::ios::beg);
+		while (getline(lines, line)) {
+			file_out << line << std::endl;
+		}
+		points.close();
+		lines.close();
+		remove("points.txt");
+		remove("lines.txt");
+		file_out << "}" << std::endl;
+		file_out.close();
+		system("\"C:\\Program Files (x86)\\Graphviz2.38\\bin\\dot.exe\" -Tpdf C:\\Users\\GSzwa\\source\\repos\\labo_algo_3\\labo_algo_3\\file.dot -o C:\\Users\\GSzwa\\source\\repos\\labo_algo_3\\labo_algo_3\\mojgraf.pdf");
+		
+		remove("file.dot");
 	}
 
 	bool erase_ptr(void)
